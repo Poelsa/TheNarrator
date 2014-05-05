d90729dc4a25d5f1b695f6454e5547efce4a2059
Erik/Wingly <Erik_the_great_one@hotmail.com>
2014-05-05 12:08:10 +0200

    Fixed draggstuff

--- Functions.js
+++ Functions.js
@@ -15,6 +15,10 @@ $(function() {
 
 	for(var prop in functions)
 		$("<div class='functions'>").html(functions[prop].id).appendTo("#Elements-Functions").click(function(e){
-			$("<div class='block ui-widget-content ui-draggable'>").html($(this).html()).appendTo("#Workspace-OnCreate");
+			$("<div class='block ui-widget-content ui-draggable'>").html($(this).html()).appendTo("#Workspace-OnCreate").draggable({
+			stack: 'div',
+			start: function(e){$(this).get(0).originalParent = $(this).parent(); $(this).parent().children().appendTo("#TempArea");},
+			stop: function(e){$(this).parent().children().appendTo($(this).get(0).originalParent);}
+		}).tooltip({ hide: { effect: "explode", duration: 1000 } });
 		});
 });
